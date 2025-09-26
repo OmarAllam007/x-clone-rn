5748e67bf5d2653a9d2d9ae336a2b8bc718a5fd2
omargarana <omar.ceh007@gmail.com>
2025-09-26 15:20:03 +0300

    wip

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,21 @@ import {ENV} from "./config/env.js";
 import {connectDB} from "./config/db.js";
 
 const app = express();
-
-connectDB();
-
 app.get("/", (req, res) => res.send("Hello World from server!"));
 
-app.listen(ENV.PORT, () => console.log('Server is running'));
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, () => console.log('Server is running'));
+
+    }catch(err) {
+        console.log(err);
+        process.exit(1);
+    }
+}
+startServer();
+
+
 
 
 
