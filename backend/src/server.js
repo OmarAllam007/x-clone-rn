24bec313eaa10870a7c441ad68ecc03a5c3bdf6d
omargarana <omar.ceh007@gmail.com>
2025-09-26 15:09:53 +0300

    db configs and models

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import {ENV} from "./config/env.js";
+import {connectDB} from "./config/db.js";
+
+const app = express();
+
+connectDB();
+
+app.get("/", (req, res) => res.send("Hello World from server!"));
+
+app.listen(ENV.PORT, () => console.log('Server is running'));
+
+
+
+
